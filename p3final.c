d2c81d56338236507ecf9ed6dc849ad88fbc7489
PrajwalBangi07 <97020307+PrajwalBangi07@users.noreply.github.com>
2022-01-16 08:47:38 +0000

    p3

---
+++ p3final.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+  int a,b ;
+  printf("enter 2 numbers\n");
+  scanf("%d %d",&a,&b);
+  if (a>b)
+  {
+    printf("A is grater \n");
+  }
+  else
+  {
+    printf("B is grater\n");
+  }
+}
