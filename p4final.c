9da1034bc2d352931f10dbc706441548ef8b23b1
PrajwalBangi07 <97020307+PrajwalBangi07@users.noreply.github.com>
2022-01-25 17:21:35 +0000

    p4 org

--- p4final.c
+++ p4final.c
@@ -2,51 +2,61 @@
 int input()
 {
   int a,b,c;
-  printf("Enter a number\t");
+  printf("Enter a number:");
   scanf("%d",&a);
   return a;
 }
 int cmp(int a,int b,int c)
-{   if(a>b && a>c)
 {
-  printf("A is greater\n");
-} 
-else if (b>a && b>c )
-{
-  printf("B is greater\n");
-}
-else if (c>a && c>b)
-{
-  printf("C is greater");
-}
-else if (a==b && b==c)
-{
-  printf("All numbers are equal");
-}
-else if (a==b && b>c)
-{
-  printf("A and B are equal and greater than C");
-}
-else if (a==c && a>b)
-{
-  printf("A and C are equal and greater than B");
-}
-else if (b==c && b>a)
-{
-  printf("B and C are equal and greater than A");
-}
-else{
-  printf("Test");
-}
-}
- int main()
+  if(a>b && a>c)
   {
-    int a,b,c;
-    a=input();
-    b=input();
-    c=input();
-    cmp(a, b, c);
+  return 1;
+  }
+  else if(b>c && b>c)
+  {
+    return 2;
+  }
+  else if(c>a && c>b)
+  {
+    return 3;
+  }
+  else if(a==b && b==c)
+  {
+    return 4;
+  }
+  else {
     return 0;
   }
-  
-  
+}
+void output(int result)
+{
+  if(result==1)
+  {
+    printf("a is greater");
+  }
+  else if(result==2)
+  {
+    printf("b is greater");
+  }
+  else if(result==3)
+  {
+    printf("c is greater");
+  }
+  else if(result==4)
+  {
+    printf("all numbers are equal");
+  }
+  else{
+    printf("invalid");
+  }
+}
+int main()
+{
+  int a,b,c,result;
+  a=input();
+  b=input();
+  c=input();
+  result=cmp(a,b,c);
+  output(result);
+  return 0;
+}
